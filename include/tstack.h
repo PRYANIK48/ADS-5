e09602349fd50fbe75d8b34cf7198893c491e3c7
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-04-19 20:50:58 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,35 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-    private:
-        T arr[size];
-        int top;
-    public:
-        TStack() :top(-1) {}
-        bool isEmpty() const {
-            return top == -1;
-        }
-        bool isFull() const {
-            return top == size - 1;
-        }
-        T get() const {
-            if (!isEmpty())
-                return arr[top];
-            else
-                throw std::string("empty");
-        }
-        void Pop() {
-            if (!isEmpty())
-                top--;
-            else
-                throw std::string("empty");
-        }
-        void Push(T item) {
-            if (!isFull())
-                arr[++top] = item;
-            else
-                throw std::string("full");
-        }
+   private:
+    T arr[size];
+    int top;
+   public:
+    TStack() :top(-1) {}
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    T get() const {
+        if (!isEmpty())
+            return arr[top];
+        else
+            throw std::string("empty");
+    }
+    void Pop() {
+        if (!isEmpty())
+            top--;
+        else
+            throw std::string("empty");
+    }
+    void Push(T item) {
+        if (!isFull())
+            arr[++top] = item;
+        else
+            throw std::string("full");
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
